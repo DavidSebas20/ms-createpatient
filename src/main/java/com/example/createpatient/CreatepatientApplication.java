8c4be711dd43cccb21f1f936311e2d1b7e8a8ae4
SrNexar <nfsalguero@uce.edu.ec>
2025-01-21 20:51:32 -0500

    initial repository

---
+++ src/main/java/com/example/createpatient/CreatepatientApplication.java
@@ -0,0 +1,13 @@
+package com.example.createpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatepatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatepatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/createpatient/CreatepatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatepatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
