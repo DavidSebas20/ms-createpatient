74f7eb193552f084e4aae9481fe958faef5a2f20
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-13 00:49:01 -0500

    ✨ feat(service): configure HASH_SERVICE_URL to use environment variable from application properties

--- src/main/java/com/example/createpatient/service/PatientService.java
+++ src/main/java/com/example/createpatient/service/PatientService.java
@@ -5,6 +5,7 @@ import com.example.createpatient.entity.HashResponse;
 import com.example.createpatient.entity.Patient;
 import com.example.createpatient.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +13,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PatientService {
-    private final String HASH_SERVICE_URL = "http://localhost:8080/hash";
+    @Value("${hash.service.url}")
+    private String HASH_SERVICE_URL;
 
     @Autowired
     private PatientRepository patientRepository;
